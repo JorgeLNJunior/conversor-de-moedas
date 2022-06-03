4936d403e58e355252cd480df55881db2f06cdfa
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-06-03 11:36:12 -0300

    feat: adicionar a navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="bg-white border border-gray-200 px-2 py-2.5 rounded">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <a href="#" class="flex items-center">
+        <img src="../assets/logo.png" class="h-10 mr-3" alt="logo" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap">
+          Conversor de moedas
+        </span>
+      </a>
+    </div>
+  </nav>
+</template>
