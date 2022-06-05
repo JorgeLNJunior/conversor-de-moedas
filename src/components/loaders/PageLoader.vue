e8a00d73da09eb86993e468d22826e83e86b02fa
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-06-05 16:56:38 -0300

    chore: criar um page loader

---
+++ src/components/loaders/PageLoader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center">
+    <!-- Outer Ring-->
+    <div class="w-20 h-20 rounded-full absolute border border-gray-200"></div>
+
+    <!-- Inner Ring -->
+    <div
+      class="w-20 h-20 rounded-full animate-spin absolute border-2 border-green-500 border-t-transparent"
+    ></div>
+  </div>
+</template>
