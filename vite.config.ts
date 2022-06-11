985d8f52ee4c32f911ba4bee1ac6803aa2ce99db
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-06-11 14:18:31 -0300

    chore: configurar alguns path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import { resolve } from 'node:path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()]
+  plugins: [vue(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@components': resolve('src', 'components'),
+      '@api': resolve('src', 'api')
+    }
+  }
 })
