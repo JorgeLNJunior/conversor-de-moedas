d3c576f41254f20ca9629b7068c34d2d6d0c4800
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-06-14 11:45:11 -0300

    refactor: remover o script da nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <nav class="bg-white border border-gray-200 px-2 py-2.5 rounded">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
