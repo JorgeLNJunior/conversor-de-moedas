335a9e9f1efa7ce3db3fc6a805e02bb8d370bc34
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-06-14 11:53:17 -0300

    fix: arredondar a nav bar somente na parte de baixo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-white border border-gray-200 px-2 py-2.5 rounded">
+  <nav class="bg-white border-b border-gray-200 px-2 py-2.5 rounded-b">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
       <a href="#" class="flex items-center">
         <img src="../assets/logo.png" class="h-10 mr-3" alt="logo" />
